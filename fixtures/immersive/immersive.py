f4839dd805662f6bf15d1cb081f1c008735ab458
evgeniy kamskiy <evgeniikqa@gmail.com>
2023-12-13 02:21:08 +0300

    refactoring

--- fixtures/immersive/immersive.py
+++ fixtures/immersive/immersive.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.fixture(scope="session", autouse=True)
 def get_object1(emdb):
-    print("create get_object1")
+    print("create object1")
     assert emdb == "emdb"
     yield "object1"
-    print("delete get_object1")
+    print("delete object1")
