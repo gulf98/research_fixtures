810d9b0e8000e9c22459469a3b1787e9a26a4a01
evgeniy kamskiy <evgeniikqa@gmail.com>
2023-12-13 03:20:13 +0300

    add get_fixture_plugins

--- conftest.py
+++ conftest.py
@@ -1,4 +1,8 @@
+import pkgutil
+
 import pytest
+
+import fixtures
 
 
 @pytest.fixture(scope="session")
@@ -13,8 +17,18 @@ def run_task_and_get_artifacts():
     return "regression"
 
 
-pytest_plugins = [
-    "fixtures.emdb",
-    "fixtures.floors.floors",
-    "fixtures.immersive.immersive"
-]
+def get_fixture_plugins():
+    plugins = []
+    prefix = fixtures.__name__ + "."
+    for _, module_name, _ in pkgutil.walk_packages(fixtures.__path__, prefix):
+        plugins.append(module_name)
+    return plugins
+
+
+pytest_plugins = get_fixture_plugins()
+
+# pytest_plugins = [
+#     "fixtures.emdb",
+#     "fixtures.floors.floors",
+#     "fixtures.immersive.immersive"
+# ]
